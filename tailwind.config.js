966684058c68c568038db18130d639e416e97b3e
xilv25 <thoriqrizqifahmi@gmail.com>
2026-09-18 13:42:06 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // <-- Ditambahkan agar folder components ikut terpindai Tailwind
   ],
   theme: {
     extend: {},
