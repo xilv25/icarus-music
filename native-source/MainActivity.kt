b59d212272f836e83e407036d33e8ce2a04e1ea7
xilv25 <thoriqrizqifahmi@gmail.com>
2026-09-19 19:35:38 +0700

    Update MainActivity.kt

--- native-source/MainActivity.kt
+++ native-source/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.icarus.music
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Jalankan Foreground Service agar OS Android tidak mematikan audio/WebView di background
+        val intent = Intent(this, MusicService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
     }
 }
